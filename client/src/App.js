df725dd1ecd90ac290dedad6a6bac1083b5d5a56
Michael Nease <michaelnease@michaelnease.com>
2020-04-26 20:14:28 -0700

    add feathers auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Auth from './contexts/authContext';
+// import PrivateRoute from './components/privateRoute';
+// import Template from './components/template';
 import Home from './pages/Home';
+// import Chat from './pages/chat/application';
+// import Dashboard from './pages/dashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Auth>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {/* <Template variant="clear"> */}
+            <Home />
+            {/* </Template> */}
+          </Route>
+          <Route exact path="/chat">
+            <p>blah</p>
+            {/* <Template variant="clear"> */}
+            {/* <Chat /> */}
+            {/* </Template> */}
+          </Route>
+          {/* <PrivateRoute exact path="/">
+            <Template variant="main">
+              <Dashboard />
+            </Template>
+          </PrivateRoute> */}
+          {/* <PrivateRoute path="/dashboard">
+            <Template variant="main">
+              <Dashboard />
+            </Template>
+          </PrivateRoute> */}
+        </Switch>
+      </Router>
+    </Auth>
   );
-}
+};
 
 export default App;
---
+++ client/src/contexts/authContext.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import client from '../app/feathers';
+export const AuthContext = React.createContext({});
+
+const Auth = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticationError, setIsAuthenticationError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const checkAuth = async () => {
+    await client
+      .reAuthenticate()
+      .then((res) => setIsAuthenticated(res.user))
+      .catch(() => setIsAuthenticated(false))
+      .then(() => setIsLoading(false));
+  };
+
+  const login = async (credentials) => {
+    try {
+      // First try to log in with an existing JWT
+      return await client.reAuthenticate();
+    } catch (error) {
+      // If that errors, log in with email/password
+      // Here we would normally show a login page
+
+      // to get the login information
+      return await client
+        .authenticate(
+          {
+            strategy: 'local',
+            ...credentials,
+          },
+          {
+            query: {
+              active: true,
+              'applications.site': 'g8keeper',
+            },
+          },
+        )
+        .then((res) => {
+          return res.user;
+        })
+        .then((user) => {
+          setIsAuthenticated(user);
+          setIsAuthenticationError(false);
+        })
+        .catch(() => {
+          setIsAuthenticated(false);
+          setIsAuthenticationError(true);
+        });
+    }
+  };
+
+  const logout = async () => {
+    await client.logout();
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        isLoading,
+        isAuthenticationError,
+        login,
+        logout,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default Auth;
